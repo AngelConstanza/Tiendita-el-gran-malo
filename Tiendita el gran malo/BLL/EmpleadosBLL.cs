1451c821fcd8b0411b66137bfb3422cf0dde6626
anaco <anaco@DESKTOP-HCN5MC5>
2022-10-15 18:08:09 -0600

    Agregar archivos de proyecto.

---
+++ Tiendita el gran malo/BLL/EmpleadosBLL.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tiendita_el_gran_malo.BLL
+{
+    class EmpleadosBLL
+    {
+    }
+}
---
+++ Tiendita el gran malo/Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Tiendita_el_gran_malo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Form proveedor = new Form2();
+            proveedor.Show();
+        }
+
+        private void local_Click(object sender, EventArgs e)
+        {
+            Form local = new Form3();
+            local.Show();
+        }
+
+        private void ganancia_Click(object sender, EventArgs e)
+        {
+            Form ganancias = new Form4();
+            ganancias.Show();
+        }
+
+        private void gastos_Click(object sender, EventArgs e)
+        {
+            Form gastos = new Form5();
+            gastos.Show();
+        }
+
+        private void empleado_Click(object sender, EventArgs e)
+        {
+            Form empleado = new Form6();
+            empleado.Show();
+        }
+    }
+}
